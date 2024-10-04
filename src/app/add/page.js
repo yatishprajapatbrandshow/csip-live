d877e7a6b8bba15d485137940c50e3e70991f0a2
atulsadiwal <atul.sadiwal@brandshow.in>
2024-10-04 19:19:57 +0530

    c

--- src/app/add/page.js
+++ src/app/add/page.js
@@ -3,14 +3,14 @@ import Header from "@/Components/Header";
 import Add from "./Add";
 export default function Home() {
   return (
-    <>
+    <section className="bg-white">
       <Header session={true} />
-      <div className='relative'>
+      <div className='relative '>
       <div className="absolute top-0 left-0 z-20">
                 <SideBar />
             </div>
         <Add />
       </div>
-    </>
+    </section>
   );
 }
