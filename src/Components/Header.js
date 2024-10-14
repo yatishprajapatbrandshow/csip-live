153a6ce21a2ae4350c7bfcae2f99daf91626eb34
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2024-10-14 18:38:35 +0530

    new push

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -206,7 +206,7 @@ const Header = ({ session = false }) => {
                                                 className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-200 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
                                             >
                                                 <MenuItem>
-                                                    <button onClick={() => router.push("/edit-profile")} className="block w-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
+                                                    <button onClick={() => router.push("/EditProfile")} className="block w-full px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100">
                                                         Setting
                                                         {/* onClick={() => LogOut()} */}
                                                     </button>
