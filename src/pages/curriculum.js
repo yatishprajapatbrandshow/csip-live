ead597bf13c7098a62e717abef5b46b339975641
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2024-10-14 11:30:38 +0530

    new push

--- src/pages/curriculum.js
+++ src/pages/curriculum.js
@@ -1,10 +1,9 @@
 "use client";
-import CardCorporate from '@/Components/CardCorporate';
 import { ArrowLeft, Check, MousePointerClick, Search, Undo2 } from 'lucide-react';
-import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { API_URL, API_URL_LOCAL } from '@/Config/Config';
 import { useSelector } from 'react-redux';
+import Header from '@/Components/Header';
 
 function Curriculum() {
   const [CurriculumData, setCurriculumData] = useState();
@@ -36,7 +35,7 @@ function Curriculum() {
 
       const responseData = await response.json();
       console.log(responseData);
-      
+
       if (responseData.status === true) {
         setcurriId(responseData.data[0].curriculum_id);
       }
@@ -77,7 +76,7 @@ function Curriculum() {
       setLoading(false); // Set loading to false after fetch is done
     }
   }
-  
+
   useEffect(() => {
     if (curriId) {
       fetchCurriculumDetails();
@@ -89,55 +88,53 @@ function Curriculum() {
     }
   }, [userData]);
 
-  const handleClick = (item, topic) => {
-    console.log(item);
-    console.log(topic);
-  }
-  
   if (loading) return <div className="text-center w-full ">Loading...</div>; // Loading state
 
   return (
-    <div className="pl-10 p-6 bg-[#F0F0F0] w-full">
-      <div className='flex items-center gap-2 mb-4 '><Undo2 className='w-5 h-5' />Go Back</div>
-      <div className='bg-white  w-full p-6'>
-        <h1 className="text-3xl font-gilBold mb-6">Curriculum</h1>
-        <h5 className="text-sm text-gray-700 mb-3">{CurriculumData?.name}</h5>
-        {
-          groupsData.map((item, index) =>
-            <ul className=' border-b-[1px] border-[#F9DAEA] mb-5' key={index}>
-              <h4 className='mb-3 text-gray-900 font-medium'>{item?.groupName}</h4>
-              {
-                item?.topics.map((ele, idx) =>
-                  <li className="list-none grid grid-cols-4 gap-10 border-t border-[#F9DAEA] py-[.25rem]">
-                    {/* Column for Check and Topic Name */}
-                    <div className="flex items-center gap-5 col-span-2">
-                      <div className="w-6 h-6 bg-gray-100 rounded-full flex items-center justify-center">
-                        <Check className="w-4 h-4 text-gray-500" />
+    <>
+      <Header />
+      <div className="pl-10 p-6 bg-[#F0F0F0] w-full">
+        <div className='flex items-center gap-2 mb-4 '><Undo2 className='w-5 h-5' />Go Back</div>
+        <div className='bg-white  w-full p-6'>
+          <h1 className="text-3xl font-gilBold mb-6">Curriculum</h1>
+          <h5 className="text-sm text-gray-700 mb-3">{CurriculumData?.name}</h5>
+          {
+            groupsData.map((item, index) =>
+              <ul className=' border-b-[1px] border-[#F9DAEA] mb-5' key={index}>
+                <h4 className='mb-3 text-gray-900 font-medium'>{item?.groupName}</h4>
+                {
+                  item?.topics.map((ele, idx) =>
+                    <li className="list-none grid grid-cols-4 gap-10 border-t border-[#F9DAEA] py-[.25rem]">
+                      {/* Column for Check and Topic Name */}
+                      <div className="flex items-center gap-5 col-span-2">
+                        <div className="w-6 h-6 bg-gray-100 rounded-full flex items-center justify-center">
+                          <Check className="w-4 h-4 text-gray-500" />
+                        </div>
+                        <p className="text-gray-700 font-medium text-sm">{ele?.topic}</p>
                       </div>
-                      <p className="text-gray-700 font-medium text-sm">{ele?.topic}</p>
-                    </div>
 
-                    {/* Column for Search Activity */}
-                    <div className="flex items-center col-span-1 text-gray-600  font-medium text-sm cursor-pointer">
-                      <Search className="text-[#9779FF] w-4 h-4 mr-2" />
-                      <span >Search Activity</span>
-                    </div>
+                      {/* Column for Search Activity */}
+                      <div className="flex items-center col-span-1 text-gray-600  font-medium text-sm cursor-pointer">
+                        <Search className="text-[#9779FF] w-4 h-4 mr-2" />
+                        <span >Search Activity</span>
+                      </div>
 
-                    {/* Column for Click to Select */}
-                    <div className="flex items-center justify-center col-span-1">
-                      <button onClick={() => { handleClick(item, ele) }} className="flex items-center bg-[#9779FF] text-white py-1 px-4  text-sm hover:bg-[#8A6CE0] transition-colors duration-200">
-                        <MousePointerClick className="w-4 h-4 mr-2" />
-                        <span>Click to Select</span>
-                      </button>
-                    </div>
-                  </li>
-                )
-              }
-            </ul>
-          )
-        }
+                      {/* Column for Click to Select */}
+                      <div className="flex items-center justify-center col-span-1">
+                        <button onClick={() => { handleClick(item, ele) }} className="flex items-center bg-[#9779FF] text-white py-1 px-4  text-sm hover:bg-[#8A6CE0] transition-colors duration-200">
+                          <MousePointerClick className="w-4 h-4 mr-2" />
+                          <span>Click to Select</span>
+                        </button>
+                      </div>
+                    </li>
+                  )
+                }
+              </ul>
+            )
+          }
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
