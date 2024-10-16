f9bb824cd630d3942ae4bbbb90784193bee3724c
yatishprajapatbrandshow <yatish.prajapat@brandshow.in>
2024-10-16 19:10:50 +0530

    new push

--- redux/store.js
+++ redux/store.js
@@ -2,10 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import sessionSlice from './actions/sessionSlice';
 import triggerSlice from './actions/triggerSlice';
 import orderIdSlice from './actions/orderIdSlice';
+import favouriteActivitySlice from './actions/favouriteActivitySlice';
 export const store = configureStore({
     reducer: {
         session: sessionSlice,
         trigger: triggerSlice,
-        orderId: orderIdSlice
+        orderId: orderIdSlice,
+        favouriteActivity: favouriteActivitySlice,
     },
 });
